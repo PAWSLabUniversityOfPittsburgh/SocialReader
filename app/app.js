16b6a79c82b3c1e20706b11041f84185f2851526
gtrivedi <trivedigaurav@gmail.com>
2015-01-26 12:17:51 -0500

    Update components structure

--- app/app.js
+++ app/app.js
@@ -2,4 +2,54 @@
     'use strict';
     
     angular.module('app', ['itemManager']);
+	
+	angular.module('itemManager', []);
+	var directivesModule = angular.module('itemManager');
+	directivesModule.directive('itemManager', itemManager);
+
+    itemManager.$inject = [];
+
+    function itemManager() {
+        var directive = {
+            link: link,
+            controller: controller,
+            controllerAs: 'vm',
+            templateUrl: 'template.html',
+            restrict: 'A'
+        };
+        
+        return directive;
+
+        function link(scope, element, attrs, controller) {
+            
+        }
+        
+        function controller() {
+            var vm = this;
+            
+            vm.updateMethod = null;
+            vm.editMethod = null;
+            
+            vm.updateItem = updateItem;
+            vm.editItem = editItem;
+            vm.respondToUpdatesWith = respondToUpdatesWith;
+            vm.respondToEditsWith = respondToEditsWith;
+                
+            function updateItem(item) {
+                vm.updateMethod(item);
+            }
+            
+            function editItem(item) {
+                vm.editMethod(item);
+            }
+            
+            function respondToUpdatesWith(method) {
+                vm.updateMethod = method;
+            }
+            
+            function respondToEditsWith(method) {
+                vm.editMethod = method;
+            }
+        }
+    }
 }());
